fb281b64d7eaa5d387234f2eac4529b3a6031c24
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-09 13:56:59 +0100

    +

--- apps/myApp/src/App.js
+++ apps/myApp/src/App.js
@@ -1,10 +1,10 @@
 import { entryPoint1 } from "@zionstate/multientry";
 import { entry2 } from "@zionstate/multientry/entry2";
 import { read } from "@zionstate/multientry/useFs";
-import hot from "@zionstate/multientry/Folder";
+import Folder, { some } from "@zionstate/multientry/Folder";
 export const App = () => "app";
 // console.log(entryPoint1);
 // console.log(entry2);
 // console.log(read);
-// console.log(hot);
+console.log(some);
 // tsc
---
+++ packages/multientry/src/Folder/some/some.ts
@@ -0,0 +1,1 @@
+export const some_v1 = "some";
