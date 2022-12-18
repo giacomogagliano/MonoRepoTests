0a82cb2a186464606d907745704807f475fedba6
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-18 15:23:10 +0100

    add Component from Jsx

--- packages/react/src/lesson1/JsxElement.tsx
+++ packages/react/src/lesson1/JsxElement.tsx
@@ -2,3 +2,10 @@ import React from "react";
 // questo è un elemento Jsx che puo essere chiamato in una
 // applicazione React
 export const JsxElement = <div>Sono un elemento JSX</div>;
+
+// un elemento JSX può essere reso facilmento un componente,
+// basta metterlo come ritorno implicito di una arrow
+// funcion, cosi:
+export const ComponentFromJsx = () => JsxElement;
+// ora questo elemento è una funzione e può essere chiamato
+// come un Componente: `<Component/>
