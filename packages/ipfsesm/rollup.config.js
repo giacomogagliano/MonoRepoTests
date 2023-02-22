e4d97bb92d73255c99957ac09408b3e0ce3c849d
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-02-22 18:38:59 +0100

    test with commonJS .. it doesnt work

--- packages/ipfsesm/rollup.config.js
+++ packages/ipfsesm/rollup.config.js
@@ -6,4 +6,25 @@ export default [
       format: "esm",
     },
   },
+  {
+    input: "dist/ipfsesm.cjs.js",
+    output: {
+      file: "dist/ipfsesm.cjs",
+      format: "cjs",
+    },
+  },
+  {
+    input: "dist/ipfsesm.cjs.dev.js",
+    output: {
+      file: "dist/ipfsesm.cjs.dev.cjs",
+      format: "cjs",
+    },
+  },
+  {
+    input: "dist/ipfsesm.cjs.prod.js",
+    output: {
+      file: "dist/ipfsesm.cjs.prod.cjs",
+      format: "cjs",
+    },
+  },
 ];
